c87705cc824e62703c750f5a6d42f1fbda193f9e
NotDead <nikindahouse@mail.ru>
2018-11-01 05:01:50 +0300

    Initial commit

---
+++ app/src/main/java/almostdone/subhunter/SubHunter.java
@@ -0,0 +1,12 @@
+package almostdone.subhunter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubHunter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
